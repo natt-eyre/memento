31c5bd51174f42312211b0ecc9834e29323537fe
Natt Eyre <terskaya.nv@gmail.com>
2016-02-28 23:20:01 +0300

    can delete entries

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "entries#index"
-  resources :entries, only: [:new, :index, :create, :show, :edit, :update]
+  resources :entries, only: [:new, :index, :create, :show, :edit, :update, :destroy]
 end
