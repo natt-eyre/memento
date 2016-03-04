07ab5bdb1e3d1eecfecad838c96a3031f76b80e5
Natt Eyre <terskaya.nv@gmail.com>
2016-03-04 23:07:37 +0300

    WIP

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -4,6 +4,6 @@ class Entry < ActiveRecord::Base
   validates_presence_of :content
   validates_presence_of :target_date
 
-  has_attached_file :picture, styles: { medium: "300x300>", thumb: "100x100>" }
+  has_attached_file :picture, styles: { medium: "300x300>", thumb: "253x200#" }
   validates_attachment_content_type :picture, content_type: /\Aimage\/.*\Z/
 end
