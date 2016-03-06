75b2fcfe035ca82c0593303032029045f1381d7c
Natt Eyre <terskaya.nv@gmail.com>
2016-03-06 19:15:23 +0300

    smtp settings

--- config/smtp.rb
+++ config/smtp.rb
@@ -1,9 +1,9 @@
 SMTP_SETTINGS = {
-  address: ENV.fetch("SMTP_ADDRESS"), # example: "smtp.sendgrid.net"
+  address: ENV.fetch("MAILGUN_SMTP_SERVER"), # example: "smtp.sendgrid.net"
   authentication: :plain,
-  domain: ENV.fetch("SMTP_DOMAIN"), # example: "heroku.com"
+  domain: ENV.fetch("MAILGUN_DOMAIN"), # example: "heroku.com"
   enable_starttls_auto: true,
-  password: ENV.fetch("SMTP_PASSWORD"),
+  password: ENV.fetch("MAILGUN_SMTP_PASSWORD"),
   port: "587",
-  user_name: ENV.fetch("SMTP_USERNAME")
+  user_name: ENV.fetch("MAILGUN_SMTP_LOGIN")
 }
